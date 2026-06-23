15effe61b8f341a72b9f8ae5f90855b4acca7907
kgsuby-gp <kgsuby@gmail.com>
2026-06-23 22:10:57 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.android.library") apply false
     id("org.jetbrains.kotlin.android") apply false
     id("dev.flutter.flutter-gradle-plugin") apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
 
 allprojects {
